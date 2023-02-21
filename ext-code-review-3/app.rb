5002ade2dcb56687849ef75da038363821ae2283
Robin <robintyler87@gmail.com>
2023-02-21 15:51:12 +0000

    Pass Test block 2

--- ext-code-review-3/app.rb
+++ ext-code-review-3/app.rb
@@ -1,12 +1,28 @@
 class SpellChecker
 
   def initialize
-    @dictionary = ["the"] #dictionary must always contain only lowercase words
+    @dictionary = ["the", "bear"] #dictionary must always contain only lowercase words
   end
 
 
   def highlight_wrong_words(string_to_test)
 
+    if string_to_test.include?(" ")
+       split_string_input = string_to_test.split
+      # print split_string_input
+      # return string_to_test
+      
+      multi_word_string_output = split_string_input.map do |word|
+        if @dictionary.include?(word)
+          word = word
+        else
+          word = "~#{word}~"
+        end 
+      end
+      print multi_word_string_output
+    return multi_word_string_output.join(" ")  
+    end
+    
     sanitized_string = string_to_test.downcase
 
     if @dictionary.include?(sanitized_string)
--- ext-code-review-3/spec/app_spec.rb
+++ ext-code-review-3/spec/app_spec.rb
@@ -1,21 +1,30 @@
 require_relative "../app.rb"
 
 RSpec.describe"Spell Checker Tests" do
+
+  let(:test_spell_checker) {SpellChecker.new}
   
   context"1st test block, single word input strings" do
     it"single word correctly spelled" do
-      test_spell_checker = SpellChecker.new
       expect(test_spell_checker.highlight_wrong_words("the")).to eq("the")
     end
     
     it"single word incorrectly spelled" do
-      test_spell_checker = SpellChecker.new
       expect(test_spell_checker.highlight_wrong_words("twhe")).to eq("~twhe~")
     end
 
     it"single word correctly spelled with random capital letters" do
-      test_spell_checker = SpellChecker.new
       expect(test_spell_checker.highlight_wrong_words("tHe")).to eq("tHe")
+    end
+  end
+  
+  context"2nd test block, 2 word inputs" do
+    it"two word string, all correct" do
+      expect(test_spell_checker.highlight_wrong_words("the bear")).to eq("the bear")
+    end
+
+    it"two word string, 1 incorrect" do
+      expect(test_spell_checker.highlight_wrong_words("thhe bear")).to eq("~thhe~ bear")
     end
   end
 
