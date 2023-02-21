585522620314052ab1f06b5e0f05999bdc0901df
Robin <robintyler87@gmail.com>
2023-02-21 14:33:00 +0000

    block 2 tests passed

--- ext-code-review-2b/app.rb
+++ ext-code-review-2b/app.rb
@@ -1,9 +1,18 @@
-# class BandPassFilter
+class BandPassFilter
 
   def modify_frequencies(frequency_array, upper_limit = 1000, lower_limit = 40)
 
-     return frequency_array
+    if upper_limit != 1000
+      return [upper_limit]
+    end
+
+    if frequency_array[0] < 1000
+      return frequency_array
+    else
+      return [1000]
+    end
+
 
   end
 
-# end
+end
--- ext-code-review-2b/spec/app_spec.rb
+++ ext-code-review-2b/spec/app_spec.rb
@@ -1,16 +1,44 @@
 require_relative "../app"
 
-RSpec.describe "First block of tests" do
-  it "return 1st single value inside default range" do
-    expect(modify_frequencies([50])).to eq ([50])
+RSpec.describe "Band Pass Filter Tests" do
+  
+  let(:testBandPassFilter) {BandPassFilter.new}
+
+  context "Block 1 in-range single values" do
+    it "return 1st single value inside default range" do
+      expect(testBandPassFilter.modify_frequencies([50])).to eq ([50])
+    end
+
+    it "return 2nd single value inside default range" do
+      expect(testBandPassFilter.modify_frequencies([100])).to eq ([100])
+    end
+
+    it "return 3rd single value inside default range" do
+      expect(testBandPassFilter.modify_frequencies([300])).to eq ([300])
+    end
+
   end
 
-  it "return 2nd single value inside default range" do
-    expect(modify_frequencies([100])).to eq ([100])
-  end
+  context "Block2 above range single values" do
+    it "return 1st value default upper bound 1000 when input >1000" do
+      expect(testBandPassFilter.modify_frequencies([1200])).to eq ([1000])
+    end
+    
+    it "return 2nd value default upper bound 1000 when input >1000" do
+      expect(testBandPassFilter.modify_frequencies([1500])).to eq ([1000])
+    end
+    
+    it "return 1st user input upper bound value when input >user upper bound input" do
+      expect(testBandPassFilter.modify_frequencies([1500], 1100)).to eq ([1100])
+    end
+    
+    it "return 2nd user input upper bound value when input >user upper bound input" do
+      expect(testBandPassFilter.modify_frequencies([1600], 1300)).to eq ([1300])
+    end
 
-  it "return 3rd single value inside default range" do
-    expect(modify_frequencies([300])).to eq ([300])
+    it "return 3rd user input upper bound value when input >user upper bound input" do
+      expect(testBandPassFilter.modify_frequencies([950], 900)).to eq ([900])
+    end
   end
 
 end
